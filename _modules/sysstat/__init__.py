944b46859dc6992059988df598031e5eb2312eb1
adamzero1 <adam.crossland@zero1.co.uk>
2018-10-16 21:26:51 +0100

    Update __init__.py

--- _modules/sysstat/__init__.py
+++ _modules/sysstat/__init__.py
@@ -1,3 +1,15 @@
+from datetime import datetime, timedelta
 
 def iowait(period=10):
-  return 'getting io wait for last: ' + str(period) + ' days'
+  
+  returnData = {
+    'period': period,
+    'results': {},
+    'average': 0
+  }
+  
+  for daysAgo in range(10, 0):
+    daysAgoDate = datetime.now() - timedelta(days=daysAgo)
+    returnData['results'][datetime.strptime(daysAgoDate, '%Y-%m-%d')] = 'blah'
+  
+  return returnData
