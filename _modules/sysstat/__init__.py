e6852b209dd63de85f418ba7cd7e52ba1bf506bf
adamzero1 <adam.crossland@zero1.co.uk>
2018-10-16 22:01:07 +0100

    Update __init__.py

--- _modules/sysstat/__init__.py
+++ _modules/sysstat/__init__.py
@@ -25,6 +25,7 @@ def iowait(period=10):
       ioWaitColumn = 0
       totalIOWait = 0
       for row in reader:
+        print('\t'.join(row))
         rowCount += 1
         if rowCount == 0:
           continue
